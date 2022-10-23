9b82e5557fc16b7d05fa998c01e22c7f9a4aa80e
Pedro Lima <pedro.oficial1969@gmail.com>
2022-10-23 13:22:16 -0300

    style(header): adiciona estilização ao componente Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { FiSearch } from 'react-icons/fi';
+import { SiGamejolt } from 'react-icons/si';
 import categories from '../../services/GameCategories';
 
 import style from '../../styles/components/_header.module.scss';
@@ -16,21 +17,29 @@ function Header({ filterGames }: Props) {
       {
         filterGames ? (
           <div className={ style.header_content_layout_filter }>
-            <h1 className={ style.title }>Free Game Now</h1>
 
-            <select>
-              <option disabled selected>Selecione</option>
-              {tenCategories.map((category, index) => (
-                // eslint-disable-next-line react/no-array-index-key
-                <option key={ index } value={ category }>{category}</option>
-              ))}
-            </select>
+            <div className={ style.title_container }>
+              <SiGamejolt className={ style.icon } />
+              <h1 className={ style.title }>
+                Free Game Now
+              </h1>
+            </div>
 
-            <select>
-              <option disabled selected>Plataforma</option>
-              <option value="pc">PC</option>
-              <option value="browser">Browser</option>
-            </select>
+            <div className={ style.selects_container }>
+              <select>
+                <option disabled selected>Selecione</option>
+                {tenCategories.map((category, index) => (
+                  // eslint-disable-next-line react/no-array-index-key
+                  <option key={ index } value={ category }>{category}</option>
+                ))}
+              </select>
+
+              <select>
+                <option disabled selected>Plataforma</option>
+                <option value="pc">PC</option>
+                <option value="browser">Browser</option>
+              </select>
+            </div>
 
             <button type="button">
               <FiSearch />
@@ -38,7 +47,13 @@ function Header({ filterGames }: Props) {
           </div>
         ) : (
           <div className={ style.header_content }>
-            <h1>Free Game Now</h1>
+
+            <div className={ style.title_container }>
+              <SiGamejolt className={ style.icon } />
+              <h1 className={ style.title }>
+                Free Game Now
+              </h1>
+            </div>
 
             <button type="button">
               <FiSearch />
