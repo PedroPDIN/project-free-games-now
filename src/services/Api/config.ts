21db68d13a1b315fffe350cf7ac954d53fdb62ac
Pedro Lima <pedro.oficial1969@gmail.com>
2022-10-27 11:34:38 -0300

    feat(enviroments): adiciona variavéis de ambiemte para guardar dados sensíveis para acessar a API

--- src/services/Api/config.ts
+++ src/services/Api/config.ts
@@ -3,8 +3,8 @@ import axios from 'axios';
 const requestApi = axios.create({
   baseURL: 'https://free-to-play-games-database.p.rapidapi.com/api',
   headers: {
-    'X-RapidAPI-Key': 'ef2bd6bfb8mshbab8e069818d343p1f8668jsnc4dc6c77ef4d',
-    'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com',
+    'X-RapidAPI-Key': import.meta.env.VITE_RAPIDAPI_KEY,
+    'X-RapidAPI-Host': import.meta.env.VITE_RAPIDAPI_HOST,
   },
 });
 
