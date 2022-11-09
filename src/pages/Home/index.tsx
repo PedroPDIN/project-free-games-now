8a93798e90b563b2ec57a5bf103b1783522a1b45
Pedro Lima <pedro.oficial1969@gmail.com>
2022-11-09 14:11:58 -0300

    feat(create page home): adiciona a base da página "Home"

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+import MainContext from '../../store/Context/Main.Context';
+import * as Components from '../../components';
+
+function Home() {
+  const { listGames } = useContext(MainContext);
+
+  return (
+    <main>
+      <Components.Header filterGames />
+
+      <div>
+        <h1>
+          Encontre e acompanhe os melhores jogos
+          <span> grátis para jogar!</span>
+        </h1>
+      </div>
+      <div>
+        <Components.CardsGames listGames={ listGames.slice(0, 10) } cardFormat="base" />
+        <Components.CardsGamesLike hasTitle={ false } />
+      </div>
+    </main>
+  );
+}
+
+export default Home;
