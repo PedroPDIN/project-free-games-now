86615c5f3fc33bfe576427b46f4c022146742b2e
Pedro Lima <pedro.oficial1969@gmail.com>
2022-12-06 10:56:39 -0300

    feat(initial footer): adiciona criação do componente "Footer"

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <h1>Aqui estais o seu componente footer</h1>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,9 +1,11 @@
 import Header from './Header';
 import CardsGames from './CardsGames';
 import CardsGamesLike from './CardsGamesLike';
+import Footer from './Footer';
 
 export {
   Header,
   CardsGames,
   CardsGamesLike,
+  Footer,
 };
